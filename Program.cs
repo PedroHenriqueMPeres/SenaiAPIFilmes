0d8bdf51a3a4ac97a1bc2a601c89585fbe8b91a3
Pedro Henrique <pedro.h.peres@aluno.senai.br>
2025-02-27 08:38:45 -0300

    fatch: subindo primeira vez

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using api_filmes_senai.Context;
+using api_filmes_senai.Interfaces;
+using api_filmes_senai.Repository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Adicionando o DbContext ao container de serviços (ajuste a string de conexão conforme necessário)
+builder.Services.AddDbContext<Filmes_Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); // Ajuste a string de conexão no appsettings.json
+
+// Injeção de dependência para o repositório
+builder.Services.AddScoped<IGeneroRepository, GeneroRepository>();
+builder.Services.AddScoped<IFilmeRepository, FilmeRepository>();
+
+// Adicionando suporte para controllers
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Mapeando os controllers
+app.MapControllers();
+
+// Iniciando a aplicação
+app.Run();
---
+++ Repository/FilmeRepository.cs
@@ -0,0 +1,100 @@
+﻿using api_filmes_senai.Context;
+using api_filmes_senai.Domains;
+using api_filmes_senai.Interfaces;
+
+namespace api_filmes_senai.Repository
+{
+    public class FilmeRepository : IFilmeRepository
+    {
+        private readonly Filmes_Context _context;
+
+        public FilmeRepository(Filmes_Context context) {
+            _context = context;
+        }
+        public void Atualizar(Guid id, Filme filme)
+        {
+            try
+            {
+                Filme filmeBuscado = _context.Filmes.Find(id)!;
+
+                if (filmeBuscado != null)
+                {
+                    filmeBuscado.IdGenero = filme.IdGenero;
+                    filmeBuscado.Titulo = filme.Titulo;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public Filme BuscarPorId(Guid id)
+        {
+            try
+            {
+                Filme filmeBuscado = _context.Filmes.Find(id)!;
+                return filmeBuscado;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Deletar(Guid id)
+        {
+            try
+            {
+                Filme filmeBuscado = _context.Filmes.Find(id)!;
+                if (filmeBuscado != null)
+                {
+                    _context.Filmes.Remove(filmeBuscado);
+
+                }
+
+                _context.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public List<Filme> Listar()
+        {
+            try
+            {
+                List<Filme> listaDeFilmes = _context.Filmes.ToList();
+                return listaDeFilmes;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public void Cadastrar(Filme novoFilme)
+        {
+            try
+            {
+                _context.Filmes.Add(novoFilme);
+                _context.SaveChanges();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Filme(Filme novoFilme)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Filme> ListarPorGenero(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
